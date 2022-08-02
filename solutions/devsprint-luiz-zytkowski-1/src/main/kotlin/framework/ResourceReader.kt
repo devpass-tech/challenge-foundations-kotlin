a014d8411670fe71d6f71751e8f94e5492188e72
pedroajs <pedroajs93@gmail.com>
2022-08-02 20:52:40 -0300

    lint

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
@@ -1,21 +1,15 @@
 package framework
 
 import framework.interfaces.IResourceReader
-import java.util.ArrayList
 
 
 object ResourceReader: IResourceReader{
 
     override fun readAsList(fileName: String): List<String> {
 
-        val list= ArrayList<String>()
-        list.add(this::class.java.getResource(fileName)!!.readText())
-        return list
+        val lines = this::class.java.getResource(fileName).readText().lines()
+
+        return lines
     }
 
-}
-
-fun main() {
-    val print = ResourceReader.readAsList("faixasINSS.txt")
-    println(print)
 }
