8613f8a10b445dc242a26890c608a58c45f296de
pedroajs <pedroajs93@gmail.com>
2022-08-03 18:07:14 -0300

    Verificando se o caminho recebido começa com um /

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
@@ -7,6 +7,16 @@ object ResourceReader: IResourceReader{
 
     override fun readAsList(fileName: String): List<String> {
 
+        val lista = mutableListOf<String>()
+
+        for(char in fileName.iterator()){
+            lista.add(char.toString())
+        }
+
+        if(lista[0] != "/") {
+           println("the file path must be initialized whith a /")
+        }
+
         val lines = this::class.java.getResource(fileName).readText().lines()
 
         return lines
