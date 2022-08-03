42fa74203fb4180790d25e4afaa4ce07a081459c
pedroajs <pedroajs93@gmail.com>
2022-08-03 18:23:55 -0300

    lint

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
@@ -7,14 +7,14 @@ object ResourceReader: IResourceReader{
 
     override fun readAsList(fileName: String): List<String> {
 
-        val lista = mutableListOf<String>()
+        val list = mutableListOf<String>()
 
         for(char in fileName.iterator()){
-            lista.add(char.toString())
+            list.add(char.toString())
         }
 
-        if(lista[0] != "/") {
-           println("the file path must be initialized whith a /")
+        if(list[0] != "/") {
+           println("the file path must be initialized with a /")
         }
 
         val lines = this::class.java.getResource(fileName).readText().lines()
