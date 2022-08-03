2a8a79dbef513e527e1e641705725b8cac5276b2
Rielly <rielly@cora.com.br>
2022-08-03 11:42:43 -0300

    [KT1-6] Remove toString

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/salarioLiquido/NetAmountValues.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/salarioLiquido/NetAmountValues.kt
@@ -7,11 +7,4 @@ data class NetAmountResult (
     var deductionIrrf: Double,
     var valueNetAmount: Double,
     var valueGrossAmount: Double
-) {
-    override fun toString(): String {
-        return "O valor bruto do seu salário é ${valueGrossAmount}." +
-                "Com o desconto do IRRF no valor ${deductionIrrf} valor equivalente ao seguinte percentual ${aliquotIrrf}, " +
-                "junto ao desconto do INSS no valor de ${aliquotInss} valor equivalente ao seguinte percentual ${deductionInss}" +
-                "O valor líquido do seu salário é de ${valueNetAmount}"
-    }
-}
+)
