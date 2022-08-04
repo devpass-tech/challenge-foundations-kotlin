e9522b1f50930392faf2347815e618d580fffb9f
pedroajs <pedroajs93@gmail.com>
2022-08-04 14:35:20 -0300

    Object que implementa a interface iresourceReader

---
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
@@ -0,0 +1,18 @@
+package framework
+
+import framework.interfaces.IResourceReader
+object ResourceReader: IResourceReader{
+    class DoesNotStartsWith(message: String) : OwnedException(message)
+    override fun readAsList(fileName: String): List<String> {
+
+        val lines: List<String>
+
+        if(fileName.startsWith("/")){
+            lines = this::class.java.getResource(fileName).readText().lines()
+        } else{
+            throw DoesNotStartsWith(message = "File path must be initialized with a /")
+        }
+
+        return lines
+    }
+}
