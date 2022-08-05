13d8c4758a259d10b1a38b37152f75e4fe34d988
elaine mattos <elaine.mattos@outlook.pt>
2022-08-05 12:12:26 -0300

    Altera nome do metodo para handleException

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/exceptions/ExceptionHandler.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/exceptions/ExceptionHandler.kt
@@ -1,7 +1,7 @@
 package framework.exceptions
 
 object ExceptionHandler {
-    fun isOwnedException(exception: Exception) {
+    fun handleException(exception: Exception) {
         when (exception) {
             is OwnedException -> println(exception.message)
             else -> println("Tivemos um problema para calcular seu salário líquido, contate os mega-devs da DevPass")
