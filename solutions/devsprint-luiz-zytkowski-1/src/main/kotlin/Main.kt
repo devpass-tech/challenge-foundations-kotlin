27f3a53d47ff4bf641416757d554ebce8d1ee278
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2022-08-05 12:31:00 -0300

    Removendo imports da main

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/Main.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/Main.kt
@@ -1,11 +1,4 @@
-import framework.OwnedException
-import framework.exceptions.EmptyUserNameException
-import framework.exceptions.InvalidRawSalaryException
-import framework.exceptions.InvalidUserNameException
-import framework.UserInputReader
-
 fun main() {
-
 }
 
 
