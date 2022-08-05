e874492d1c7d53efa243dbf42dc8d59e9861a19b
talu <talita.santos96@hotmail.com>
2022-08-05 19:57:42 -0300

    [KT1-2] Correções na open class Loggable

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/Loggable.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/Loggable.kt
@@ -1,35 +1,18 @@
 package framework
 
-interface ILogger {
-    fun info(message: String)
-    fun warn(message: String)
-    fun error(message: String)
-}
-open class Loggable (val logger: String)
+import framework.interfaces.ILogger
+open class Loggable {
+    val logger = object : ILogger {
+        override fun error(message: String) {
+            println("[Error] $message")
+        }
 
-class Logger: Loggable(logger = "This is a logger message - for scratch and test purposes"), ILogger {
+        override fun info(message: String) {
+            println("[Info] $message")
+        }
 
-    override fun error(message: String) {
-        println("Error message - You found an error message.")
+        override fun warn(message: String) {
+            println("[Warn] $message")
+        }
     }
-
-    override fun info(message: String) {
-        println("Info message - You found an information message.")
-    }
-
-    override fun warn(message: String) {
-        println("Warn message - You found a warning message.")
-    }
-}
-
-fun main() {
-    val loggable = Loggable("This is a loggable message - for scratch and test purposes")
-    println(loggable.logger)
-    println("\n")
-
-    val logger = Logger()
-    println(logger.logger)
-    logger.error("")
-    logger.info("")
-    logger.warn("")
 }
