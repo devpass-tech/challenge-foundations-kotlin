644dfce04734d4f4a21f591c7ec08e5c464f67ba
pedroajs <pedroajs93@gmail.com>
2022-08-09 17:26:32 -0300

    mudando o logger de property para object

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/Loggable.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/Loggable.kt
@@ -2,7 +2,7 @@ package framework
 
 import framework.interfaces.ILogger
 open class Loggable {
-    val logger = object : ILogger {
+    object logger: ILogger {
         override fun error(message: String) {
             println("[Error] $message")
         }
