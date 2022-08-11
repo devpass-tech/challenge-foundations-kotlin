2b3121f22759758336fbe1cff51b08d308978698
Rielly <rielly@cora.com.br>
2022-08-10 21:03:23 -0300

    [KT1-18] Refactor ResourceReader.kt

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/framework/ResourceReader.kt
@@ -3,16 +3,17 @@ package framework
 import framework.exceptions.DoesNotStartsWithException
 import framework.interfaces.IResourceReader
 
-object ResourceReader: IResourceReader{
+object ResourceReader : IResourceReader, Loggable() {
 
     override fun readAsList(fileName: String): List<String> {
 
-        val lines: List<String>
+        var lines: List<String> = listOf<String>()
 
-        if(fileName.startsWith("/")){
-            lines = this::class.java.getResource(fileName).readText().lines()
-        } else{
-            throw DoesNotStartsWithException(message = "File path must be initialized with a /")
+        try {
+            this::class.java.getResource(fileName).readText().lines()
+        } catch (e: Exception) {
+            logger.error("File path must be initialized with a /")
+            throw DoesNotStartsWithException("File path must be initialized with a /")
         }
 
         return lines
