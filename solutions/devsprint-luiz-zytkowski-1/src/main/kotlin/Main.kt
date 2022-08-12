f5c640f61cc23f5db0b35dd8a403648132a70418
livioqueiroz <queiroz.livio@gmail.com>
2022-08-12 19:31:34 -0300

    Inserida Função que calcula o salário liquido

--- solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/Main.kt
+++ solutions/devsprint-luiz-zytkowski-1/src/main/kotlin/Main.kt
@@ -1,25 +1,20 @@
+import framework.LiquidSalaryCalculator
 import framework.UserInputReader
+import framework.exceptions.ExceptionHandler
 import framework.extensionFunctions.toPrettyPrint
-import inss.INSSCalculator
-import irrf.IRRFCalculator
-import salarioLiquido.NetAmountResult
 
 fun main() {
 
-    val userName = UserInputReader.readUserName()
-    val grossAmount = UserInputReader.readRawSalary()
+    try {
+        val userName = UserInputReader.readUserName()
+        val grossAmount = UserInputReader.readRawSalary()
 
-    val inssDeduction = INSSCalculator().totalDueValue(grossAmount)
-    val irrfDeduction = IRRFCalculator().calculateIRRF((grossAmount - inssDeduction))
+        val liquidSalaryCalculator = LiquidSalaryCalculator()
+        val netAmountResult = liquidSalaryCalculator.calculateLiquidSalary(grossAmount)
 
-    val netAmount = grossAmount - inssDeduction - irrfDeduction
-
-    println("Olá, $userName, o seu salário líquido é o seguinte: ")
-    NetAmountResult(
-        deductionInss = inssDeduction,
-        deductionIrrf = irrfDeduction,
-        valueNetAmount = netAmount,
-        valueGrossAmount = grossAmount,
-    ).toPrettyPrint()
-
+        println("Olá, $userName, o seu salário líquido é o seguinte: ")
+        netAmountResult.toPrettyPrint()
+    } catch (e: Exception) {
+        ExceptionHandler.handleException(e)
+    }
 }
