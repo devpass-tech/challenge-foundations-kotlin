a922f13428324986543f6f45755783f90666b6cf
Wesley Paternezi <12wesleyp@gmail.com>
2023-01-17 19:57:37 -0300

    fix fun validateInputSalary where salary must be higher than 0

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/Main.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/Main.kt
@@ -10,7 +10,7 @@ fun main() {
 }
 
 fun validateInputSalary(salary : String) : Double{
-    if (salary.toDoubleOrNull() != null){
+    if (salary.toDoubleOrNull() != null && salary.toDouble() > 0.0){
         return salary.toDouble()
     } else{
         throw InvalidRawSalaryException("please insert a valid salary")
