7705d9b16ce66188da233056194f0f8e4f99257f
bino1 <r.asera@outlook.com>
2023-01-20 13:27:57 -0300

    Implementando extension function para fazer um print do resultado do cálculo.

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/salarioLiquido/ResultadoSalarioLiquido.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/salarioLiquido/ResultadoSalarioLiquido.kt
@@ -1,8 +1,13 @@
 package salarioLiquido
 
-data class ResultadoSalarioLiquido (
+data class NetSalaryResult (
     val grossSalary: Double,
     val netSalary: Double,
     val discountINSS: Float,
     val discountIRRF: Float
 )
+
+fun resultSalary(args: Array<String>) {
+    val salary = resultSalary(args)
+    println(salary)
+}
