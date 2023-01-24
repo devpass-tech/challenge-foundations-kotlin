4829119da601d01f4ce2da0099d381a1773ed430
bino1 <r.asera@outlook.com>
2023-01-24 09:56:10 -0300

    Criando fun de validacao ara salario e usuario

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/OwnedException.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/OwnedException.kt
@@ -7,3 +7,7 @@ class ThereIsNoValueFromIndex(message: String) : OwnedException(message)
 class NotFoundException(message: String) : OwnedException(message)
 
 class InvalidRawSalaryException (message : String) : OwnedException(message)
+
+class InvalidInputFormatNameException (message : String) : OwnedException(message)
+
+class InvalidInputFormatSalaryException (message : String) : OwnedException(message)
--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/salarioLiquido/UserInputReader.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/salarioLiquido/UserInputReader.kt
@@ -2,33 +2,39 @@
 
 package salarioLiquido
 
+import framework.exceptionhandler.InvalidInputFormatNameException
+import framework.exceptionhandler.InvalidInputFormatSalaryException
 import framework.exceptionhandler.InvalidRawSalaryException
-import framework.exceptionhandler.OwnedException
 import java.util.Scanner
 
 object UserInputReader {
 
-    fun readRawSalary(): Double {
+    fun readRawSalary() {
 
-        val reader = Scanner(System.`in`)
+        val readerSalary = Scanner(System.`in`)
         print("Inform gross salary: ")
-        val salary = reader.nextDouble()
 
-        return if (salary > 0.0) {
-            return salary
-        } else {
+        return validateSalary(readerSalary.nextDouble())
+    }
+
+    private fun validateSalary(readerSalary: Double?) {
+        if (readerSalary == null)
             throw InvalidRawSalaryException("Invalid data, please enter a valid value")
-        }
+            throw InvalidInputFormatSalaryException("Negative salary, please enter a valid value")
+            throw InvalidInputFormatSalaryException("Salary with non-numeric values, please enter a valid value")
     }
+}
 
-    fun readUserName(): String {
-        print("Enter username: ")
-        val userName = readLine()
+fun readUserName(): String? {
+    print("Enter username: ")
+    val userName = readLine()
 
-        return if (userName != null) {
-            return userName
-        } else {
-            throw OwnedException("Informe um usuario valido")
-        }
-    }
+    validateUserName(userName)
+    return userName
+}
+
+private fun validateUserName(userName: String?): Boolean {
+    if (userName == null)
+        throw InvalidInputFormatNameException("Blank name, please enter a valid username")
+        throw InvalidInputFormatNameException("Name with numbers, please enter a valid username")
 }
