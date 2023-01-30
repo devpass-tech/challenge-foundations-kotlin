ea6cad1305f4799d8e82348950557ff55ae26388
Wesley Paternezi <12wesleyp@gmail.com>
2023-01-30 09:21:08 -0300

    add try catch at main fix switch case at ExceptionHandler

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/ExceptionHandler.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/ExceptionHandler.kt
@@ -6,12 +6,30 @@ object ExceptionHandler : Loggable() {
     fun handler(ex: Exception) {
         logger.error(ex.message.toString())
         when (ex) {
+            is ThereIsNoValueFromIndex -> {
+                logger.error("Indice do valor nao foi encontrado")
+            }
+
+            is NotFoundException -> {
+                logger.info("Se o arquivo estiver no resources não esqueça da / meu caro dev")
+            }
+
             is InvalidRawSalaryException -> {
                 logger.error("Valor precisa ser numerico ex: 3559.50")
             }
-            is NotFoundException -> {
-                logger.error("Se o arquivo estiver no resources não esqueça da / meu caro dev")
+
+            is InvalidInputFormatNameException -> {
+                logger.error("Por favor insira um nome válido")
             }
+
+            is InvalidInputFormatSalaryException -> {
+                logger.error("Salary with non-numeric values, please enter a valid value")
+            }
+
+            !is OwnedException -> {
+                logger.error("Houve um erro inesperado, por favor tente novamente")
+            }
+
             else -> {
                 logger.error("Tivemos um problema para calcular seu salário líquido, contate os mega-devs da DevPass")
             }
