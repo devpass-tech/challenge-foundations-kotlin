ef26916827ea73d5220b09250396e250b7b47764
Wesley Paternezi <12wesleyp@gmail.com>
2023-01-30 14:53:45 -0300

    fix message

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/ExceptionHandler.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/framework/exceptionhandler/ExceptionHandler.kt
@@ -4,34 +4,40 @@ import framework.Loggable
 
 object ExceptionHandler : Loggable() {
     fun handler(ex: Exception) {
-        logger.error(ex.message.toString())
         when (ex) {
             is ThereIsNoValueFromIndex -> {
-                logger.error("Indice do valor nao foi encontrado")
+                logger.error("Indice do valor nao foi encontrado.\n " +
+                        "[Message: ${ex.message.toString()}]")
             }
 
             is NotFoundException -> {
-                logger.info("Se o arquivo estiver no resources não esqueça da / meu caro dev")
+                logger.info("Se o arquivo estiver no resources não esqueça da / meu caro dev\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
 
             is InvalidRawSalaryException -> {
-                logger.error("Valor precisa ser numerico ex: 3559.50")
+                logger.error("Valor precisa ser numerico ex: 3559.50\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
 
             is InvalidInputFormatNameException -> {
-                logger.error("Por favor insira um nome válido")
+                logger.error("Por favor insira um nome válido\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
 
             is InvalidInputFormatSalaryException -> {
-                logger.error("Salary with non-numeric values, please enter a valid value")
+                logger.error("Salary with non-numeric values, please enter a valid value\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
 
             !is OwnedException -> {
-                logger.error("Houve um erro inesperado, por favor tente novamente")
+                logger.error("Houve um erro inesperado, por favor tente novamente\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
 
             else -> {
-                logger.error("Tivemos um problema para calcular seu salário líquido, contate os mega-devs da DevPass")
+                logger.error("Tivemos um problema para calcular seu salário líquido, contate os mega-devs da DevPass\n" +
+                        " [Message: ${ex.message.toString()}]")
             }
         }
     }
